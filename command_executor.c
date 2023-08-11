3d7fc09d4c88522ebe35ef8d627e7a6e127269d9
braveking12 <davidkadofficial@gmail.com>
2023-08-11 01:47:08 -0700

    simple shell0.3, simple shell0.4 command_executor file

---
+++ command_executor.c
@@ -0,0 +1,53 @@
+#include "shell.h"
+
+/**
+ * execute_command - Execute a command along with its full path variables.
+ * @data: Pointer to the program's data structure.
+ * Return: 0 on success, -1 on failure.
+ */
+int execute_command(data_of_program *data)
+{
+	int outcome = 0, status;
+	pid_t pid;
+
+	outcome = execute_builtin_list(data);
+	if (outcome != -1)
+	{
+		return (outcome);
+	}
+	outcome = find_in_path(data);
+	if (outcome)
+	{
+		return (outcome);
+	}
+	else
+	{
+			pid = fork();
+		if (pid == -1)
+		[
+			perror(data->command_name), exit(EXIT_FAILURE);
+		}
+		if (pid == 0)
+		{
+			outcome = execve(data->tokens[0], data->tokens, data->env);
+			if (outcome == -1)
+			{
+				perror(data->command_name), exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			wait(&status);
+			if (WIFEXITED(status))
+			{
+				errno = WEXITSTATUS(status);
+			}
+			else if (WIFSIGNALED(status))
+			{
+				errno = 128 + WTERMSIG(status);
+			}
+		}
+	}
+	return (0);
+}
+
