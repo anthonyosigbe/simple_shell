56a5f5ad7b73532c751b97b3183ce1370edd7b50
braveking12 <davidkadofficial@gmail.com>
2023-08-11 14:34:41 -0700

    update in the interactive_shell file new funtion added

--- interactive_shell.c
+++ interactive_shell.c
@@ -5,7 +5,6 @@
  * @argc: Number of command line values.
  * @argv: Command line values.
  * @env: Environment variables.
- * using if statement
  * Return: Zero on success.
  */
 int main(int argc, char *argv[], char *env[])
@@ -13,18 +12,76 @@ int main(int argc, char *argv[], char *env[])
 	data_of_program data_struct = {NULL}, *data = &data_struct;
 	char *prompt = "";
 
-		initialize_data(data, argc, argv, env);
+	initialize_data(data, argc, argv, env);
 
-		signal(SIGINT, handle_ctrl_c);
+	signal(SIGINT, handle_ctrl_c);
 
 	if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO) && argc == 1)
 	{
-	/* Running in the terminal, interactive mode */
+		/* Running in the terminal, interactive mode */
 		errno = 2;
 		prompt = PROMPT_MSG;
 	}
-		errno = 0;
-		run_shell_loop(prompt, data);
+	errno = 0;
+	run_shell_loop(prompt, data);
 	return (0);
+}
+/**
+ * handle_ctrl_c - Handles SIGINT signal (ctrl + c).
+ * @opr: Unused option.
+ */
+void handle_ctrl_c(int opr __attribute__((unused)))
+{
+	print_string("\n");
+	print_string(PROMPT_MSG);
+}
+
+/**
+ * initialize_data - Initializes program data structure.
+ * @data: Pointer to data structure.
+ * @argv: Array of program arguments.
+ * @env: Environment variables.
+ * @argc: Number of command line values.
+ */
+void initialize_data(data_of_program *data, int argc, char *argv[], char **env)
+{
+	int i = 0;
+
+	data->program_name = argv[0];
+	data->input_line = NULL;
+	data->command_name = NULL;
+	data->exec_counter = 0;
+
+	if (argc == 1)
+		data->file_descriptor = STDIN_FILENO;
+	else
+	{
+		data->file_descriptor = open(argv[1], O_RDONLY);
+		if (data->file_descriptor == -1)
+		{
+			print_to_stderr(data->program_name);
+			print_to_stderr(": 0: Can't open ");
+			print_to_stderr(argv[1]);
+			print_to_stderr("\n");
+			exit(127);
+		}
+	}
+	data->tokens = NULL;
+	data->env = malloc(sizeof(char *) * 50);
+	if (env)
+	{
+		for (i = 0; env[i]; i++)
+		{
+			data->env[i] = duplicate_string(env[i]);
+		}
+	}
+	data->env[i] = NULL;
+	env = data->env;
+
+	data->alias_list = malloc(sizeof(char *) * 20);
+	for (i = 0; i < 20; i++)
+	{
+		data->alias_list[i] = NULL;
+	}
 }
 
