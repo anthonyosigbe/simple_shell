04f234976d5f1b31fafb63122e8609279c21e4a8
braveking12 <davidkadofficial@gmail.com>
2023-08-14 09:57:45 -0700

    update two functions added for Create or update an environment variable, Delete an environment variable

--- operations_env.c
+++ operations_env.c
@@ -22,23 +22,19 @@ int display_environment(data_of_program *data)
 			/* Check for the presence of '=' character */
 			if (data->tokens[1][index] == '=')
 			{
-				/* Create a temporary copy of the variable value */
 				var_copy = calculate_string_length(get_environment_key(cpname, data));
 				if (var_copy != NULL)
 					set_environment_key(cpname, data->tokens[1] + index + 1, data);
 
-				/* Print the environment */
 				print_environment(data);
 
 				if (get_environment_key(cpname, data) == NULL)
 				{
-					/* Print the variable if it's absent in the environment*/
 					print_string(data->tokens[1]);
 					print_string("\n");
 				}
 				else
 				{
-					/* Restore the previous value of the variable */
 					set_environment_key(cpname, var_copy, data);
 					free(var_copy);
 				}
@@ -50,6 +46,56 @@ int display_environment(data_of_program *data)
 		perror(data->command_name);
 		errno = 127;
 	}
+	return (0);
+}
+/**
+ * set_environment - Modify the environment variable.
+ * @data: Pointer to the program's data structure.
+ *
+ * Return: Zero on success, or a non-zero number if specified as arguments.
+ */
+
+int set_environment(data_of_program *data)
+{
+	/* Validate provided arguments */
+	if (data->tokens[1] == NULL || data->tokens[2] == NULL)
+	return (0);
+	if (data->tokens[3] != NULL)
+	{
+		errno = E2BIG;
+		perror(data->command_name);
+	return (5);
+	}
+
+	/* Set the environment variable */
+		set_environment_key(data->tokens[1], data->tokens[2], data);
+
+	return (0);
+}
+
+
+/**
+ * unset_environment - Clear the specified environment variable.
+ * @data : Pointer to the program's data structure.
+ * using if statement
+ * Return: Zero on success, or a non-zero number if provided as arguments.
+ */
+
+int unset_environment(data_of_program *data)
+{
+	/* Validate provided arguments */
+	if (data->tokens[1] == NULL)
+		return (0);
+	if (data->tokens[2] != NULL)
+	{
+		errno = E2BIG;
+		perror(data->command_name);
+		return (5);
+	}
+
+	/* Remove the environment variable */
+	remove_environment_key(data->tokens[1], data);
+
 	return (0);
 }
 
