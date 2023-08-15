40af51a3b294ab04ee124901e2772bfc204a2ed0
Anthony Osigbe <thronesofglory@gmail.com>
2023-08-15 06:56:54 -0700

    get_alias_name function created and updated

--- alias_operations.c
+++ alias_operations.c
@@ -36,3 +36,33 @@ int display_aliases(data_of_program *data, char *alias)
 	}
 	return (0);
 }
+
+/**
+ * get_alias_name - Retrieve the value of an alias.
+ * @data: Pointer to the program's data structure.
+ * @alias: Name of the requested alias.
+ *
+ * Return: Pointer to the alias value, or NULL if not found.
+ */
+char *get_alias_name(data_of_program *data, char *alias)
+{
+	int index, alias_length;
+
+	/* Validate the arguments */
+	if (alias == NULL || data->alias_list == NULL)
+		return (NULL);
+
+	alias_length = calculate_string_length(alias);
+	for (index = 0; data->alias_list[index]; index++)
+	{
+		/* Iterate through the alias list and check for matching alias names */
+		if (compare_strings(alias, data->alias_list[index], alias_length) &&
+				data->alias_list[index][alias_length] == '=')
+		{
+			/* Return the value of the alias */
+			return (data->alias_list[index] + alias_length + 1);
+		}
+	}
+	/* Return NULL if alias not found */
+	return (NULL);
+}
